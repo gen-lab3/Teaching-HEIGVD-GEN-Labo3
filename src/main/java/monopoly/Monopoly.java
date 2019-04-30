b13918a579587abf4d38ac565ac5fdad2a713dcd
Julien Quartier <julien.quartier@bluewin.ch>
2019-04-30 13:02:42 +0200

    Merge Monopoly

--- src/main/java/monopoly/Monopoly.java
+++ src/main/java/monopoly/Monopoly.java
@@ -1,7 +1,25 @@
 package monopoly;
 
+import javax.swing.text.StyledEditorKit;
+import java.util.ArrayList;
+
 public class Monopoly {
+
+    private ArrayList<Die> dice;
+    private Board board;
+    private ArrayList<Player> players;
+
     public static void main(String[] args) {
-        System.out.println("Hello, monde!");
+        new Monopoly();
+    }
+
+    public Monopoly(){
+        dice = new ArrayList<>();
+        players = new ArrayList<>();
+
+        dice.add(new Die());
+        dice.add(new Die());
+
+
     }
 }
